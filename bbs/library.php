75ecf8d41f46024318bcba54ad7b2c233798c95e
Ryuta-Nagaoka <97508941+Ryuta-Nagaoka@users.noreply.github.com>
2022-03-22 20:23:08 +0900

    php_init_production

---
+++ bbs/library.php
@@ -0,0 +1,19 @@
+<?php
+
+// htmlspecialcharsの関数
+function h($value) {
+  return htmlspecialchars($value, ENT_QUOTES);
+}
+
+// DBへの接続
+function dbconnect() {
+  $db = new mysqli('localhost', 'root', 'root', 'min_bbs');
+  if (!$db) {
+    die($db->error);
+  }
+
+  return $db;
+}
+  
+
+?>
